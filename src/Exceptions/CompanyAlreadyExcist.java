9cd244ce3b343e5f9242f125d3e838d0c89929ea
Vlad <46108802+VladL221@users.noreply.github.com>
2023-07-26 13:29:13 +0300

    Update CompanyAlreadyExcist.java

--- src/Exceptions/CompanyAlreadyExcist.java
+++ src/Exceptions/CompanyAlreadyExcist.java
@@ -6,6 +6,7 @@ public class CompanyAlreadyExcist extends Exception {
 	public CompanyAlreadyExcist() {
 		super("The company already excist with the following email or name please try again");
 	}
+
 	
 	
 }
